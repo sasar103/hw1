53ef4f2aae6ae0e1a33b393d6fafdb496f1ba45b
sasar103 <Sanjoli.Saraswat@uni-duesseldorf.de>
2015-11-04 13:11:49 +0100

    updated my homework

--- fibonacci.cxx
+++ fibonacci.cxx
@@ -1,1 +1,17 @@
+#include<iostream>
+using namespace std;
 
+int main (){
+int N,f1,f2,f;
+cout<< "N" <<endl;
+cin>>N;
+f1=0;
+f2=1;
+for(int i=0; i<N-1; i++){
+  f=f1+f2;
+  f1=f2;
+  f2=f;
+ }
+cout<<f<<endl;
+
+}
